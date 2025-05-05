b2e22a80ca3b97e694967065f010b81cbbae1da7
joy935 <tupaiv@gmail.com>
2025-05-05 11:31:12 +0200

    added com

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// display the current time
 Console.WriteLine($"The current time is {DateTime.Now}");
+// calculate and display the days until Christmas
 int daysUntilChristmas = (new DateTime(DateTime.Now.Year, 12, 25) - DateTime.Now).Days;
 Console.WriteLine($"There are {daysUntilChristmas} days until the next Christmas");
